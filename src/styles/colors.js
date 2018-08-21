199dc6494f8610964a3ea0487de95e18e42554de
Matheus Campos <silva.campos.matheus@gmail.com>
2018-08-21 15:04:19 -0300

    Define os estilos do projeto

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+
+  transparent: 'transparent',
+  darkTransparent: 'rgba(0, 0, 0, 0.6)',
+  whiteTransparent: 'rgba(255, 255, 255, 0.3)',
+};
---
+++ src/styles/general.js
@@ -0,0 +1,12 @@
+import colors from './colors';
+import metrics from './metrics';
+
+
+export default {
+  container: {
+    borderRadius: metrics.baseRadius,
+    backgroundColor: colors.white,
+    padding: metrics.basePadding,
+    marginTop: metrics.baseMargin,
+  },
+};
