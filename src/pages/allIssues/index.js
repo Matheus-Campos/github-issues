565e9d80342641413fe585deeb5074ed05299034
Matheus Campos <silva.campos.matheus@gmail.com>
2018-08-21 15:05:06 -0300

    Cria todas as páginas que serão usadas no aplicativo

---
+++ src/pages/allIssues/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AllIssues = () => (
+  <View />
+);
+
+export default AllIssues;
---
+++ src/pages/openIssues/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const OpenIssues = () => (
+  <View />
+);
+
+export default OpenIssues;
